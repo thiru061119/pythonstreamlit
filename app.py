d3df3bd24f44b4000e3ac96d5909d33017d1e649
thiru061119 <151013331+thiru061119@users.noreply.github.com>
2026-02-05 22:09:41 +0530

    User Table Creation,Insertion,Retrive

--- app.py
+++ app.py
@@ -1,6 +1,43 @@
 import streamlit as st
-st.title('my first web app')
-name=st.text_input("enter your name")
-age=st.number_input("enter your age",min_value=21, max_value=35)
-if st.button("Submit"):
-    st.success(f"Hello {name}, you are {age} years old!")
+import sqlite3
+
+# Connect to database
+conn = sqlite3.connect("database.db", check_same_thread=False)
+cursor = conn.cursor()
+
+# Create table if not exists
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    age INTEGER,
+    Doe Dafault datetime
+)
+""")
+conn.commit()
+
+st.title("User Data")
+
+# Form
+with st.form("user_form"):
+    name = st.text_input("Enter Name")
+    age = st.number_input("Enter Age", min_value=21, max_value=45)
+    submit = st.form_submit_button("Save")
+
+# Save to DB
+if submit:
+    if name:
+        cursor.execute(
+            "INSERT INTO users (name, age) VALUES (?, ?)",
+            (name, age)
+        )
+        conn.commit()
+        st.success("Saved to database ✅")
+    else:
+        st.error("Name is required")
+
+# Show data
+st.subheader("Saved Users")
+rows = cursor.execute("SELECT name, age FROM users").fetchall()
+st.table(rows)
+
